7e9cc2162f3ba893a0c5fff219a394bceaf8cfc7
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-10-15 17:59:31 -1000

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'eaccelerator'
+maintainer 'Leonard TAVAE'
+maintainer_email 'leonard.tavae@informatique.gov.pf'
+license 'Apache 2.0'
+description 'Installs/Configures eaccelerator'
+long_description 'Installs/Configures eaccelerator'
+version '0.1.0'
